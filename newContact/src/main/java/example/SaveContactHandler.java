d48588c31350964d66160039819b8143e9307e0c
Lucas Rango <lucas.rango@uala.com.ar>
2020-09-17 15:52:21 -0300

    Removed unused imports

--- newContact/src/main/java/example/SaveContactHandler.java
+++ newContact/src/main/java/example/SaveContactHandler.java
@@ -1,15 +1,10 @@
 package example;
 
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class SaveContactHandler extends ContactHandler<Contact, String> {
 
